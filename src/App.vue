4112feb743dd07e503fb57f0cb879bfea4dc4ba3
uchinka <uchinkaa@gmail.com>
2018-11-17 10:42:24 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div id="homepage">
+      <home-page/>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import HomePage from './components/HomePage'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    HomePage
+  }
+}
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Roboto+Condensed&subset=vietnamese');
+  @import url('https://fonts.googleapis.com/css?family=Roboto');
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Roboto Condensed', sans-serif;
+  }
+  #homepage {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-image: url('./assets/background.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 50%;
+    overflow: hidden;
+    min-height: 100vh;
+    user-select: none;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+  }
+</style>
+
---
+++ src/components/HomePage.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div class="logo">
+            <a href="#">
+                <img src="../assets/logo.png" alt="logo">
+            </a>
+        </div>
+        <s-v-g-component/>
+    </div>
+</template>
+<script>
+    import SVGComponent from './SVGComponent';
+    export  default {
+        name: 'HomeComponent',
+        components: {
+            SVGComponent
+        }
+    }
+</script>
+
+<style>
+    .logo {
+        position: absolute;
+        top: 1.3rem;
+        left: 1.3rem;
+        z-index: 444;
+    }
+    .logo > a > img {
+        width: 130px;
+    }
+
+
+    @media screen and (max-width: 320px) {
+        .logo {
+            text-align: center;
+            padding-top: 20px;
+            display: none;
+        }
+    }
+
+</style>
